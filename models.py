3e941976cba8e841d94f80c7eb30c9ac6b58a33c
davidKro <93348200+davidKro@users.noreply.github.com>
2022-09-23 17:52:24 -0400

    feature/10: Add listing model 📄 (#13)
    * adds models.py with listing class
    
    * Update to pull request
    
    * Added blank line at end of file
    
    * Added blank line at end of file
    
    * Follow PEP8 Conventions

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,7 +10,6 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     # An object to display a user entity
-    
     """
     languages -- A list of languages the user speaks
     listings -- A list of listings the user has 
@@ -24,4 +25,35 @@ class User(db.Model):
     about = db.Column(db.String(2000), nullable=False)
     location = db.Column(db.String(150), nullable=False)
     response_rate = db.Column(db.Integer, nullable=False)
+    listings = db.relationship('Listing', backref='users')
+    
+    
+class Listing(db.Model):
+    """
+    An Object to Express a property listing.\n
 
+    Properties:\n
+    listing_id-- The integer ID for the listing\n
+    address   -- A String in the form country,province,city,street,number \n
+    user_id   -- The user who that made the listing\n
+    price   -- A float, price per night of the property\n
+    dscrpt  -- A string describing the property\n
+    rating  -- A float between 0-5
+    pics    -- A list of image files of the property\n
+    aval    -- A list of pairs of days
+    reviews -- A list of "Review" objects\n
+    """
+
+    __tablename__ = 'listings'
+
+    listing_id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(200), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    price = db.Column(db.Integer)
+    description = db.Column(db.String(500))
+    rating = db.Column(db.Integer)
+    reviews = db.relationship('Review', backref='reviews', lazy=True)
+    picture = db.Column(db.String(200))
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    
