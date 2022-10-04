45032f362ecc16ebe7f57166667c664235539ac9
FlatPaper <20js30@queensu.ca>
2022-10-03 21:32:31 -0400

    Update models to fit sprint 2's requirements (#22)

--- qbay/models.py
+++ qbay/models.py
@@ -11,24 +11,27 @@ db = SQLAlchemy(app)
 class User(db.Model):
     # An object to display a user entity
     """
-    languages -- A list of languages the user speaks
-    listings -- A list of listings the user has
-    pfp -- A profile picture on the user's profile
+    user_id: id of the user (unique)
+    email: a string that respresents the users email
+    password: the users password
+    billing_address: billing address of the user
+    postal_code: postal code of the user
+    balance: balance of the user
+    listings: all listings the user owns
+    reviews: all reviews posted by the user
+    bookings: all bookings related to the user
     """
-
     __tablename__ = 'users'
 
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    phone_num = db.Column(db.Integer, unique=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
-    about = db.Column(db.String(2000), nullable=False)
-    location = db.Column(db.String(150), nullable=False)
-    response_rate = db.Column(db.Integer, nullable=False)
-    user_balance = db.Column(db.Integer, nullable=False)
-    listings = db.relationship('Listing', backref='user')
-    reviews = db.relationship('Review', backref='user')
-    transaction = db.relationship('Transaction', backref='user')
+    password = db.Column(db.String(120), nullable=False)
+    billing_address = db.Column(db.String(120), nullable=False)
+    postal_code = db.Column(db.String(7), nullable=False)
+    balance = db.Column(db.Integer, nullable=False)
+    listings = db.relationship('Listing')
+    reviews = db.relationship('Review')
+    bookings = db.relationship('Booking')
 
 
 class Listing(db.Model):
@@ -36,30 +39,29 @@ class Listing(db.Model):
     An Object to Express a property listing.\n
 
     Properties:\n
-    listing_id -- The integer ID for the listing\n
-    address   -- A String in the form country,province,city,street,number \n
-    user_id   -- The user who that made the listing\n
-    price   -- A float, price per night of the property\n
-    dscrpt  -- A string describing the property\n
-    rating  -- A float between 0-5
-    pics    -- A list of image files of the property\n
-    aval    -- A list of pairs of days
-    reviews -- A list of "Review" objects\n
+    listing_id: The integer ID for the listing\n
+    title: title of the listing
+    description: description of the listing
+    price: price of the listing
+    last_modified_date: last date the listing was modified
+    address: A String in the form country,province,city,street,number
+    user_id: The user who that made the listing
+    price: A float, price per night of the property
+    reviews: all the reviews linked to this listing
+    bookings: all the bookings linked to this listing
     """
-
     __tablename__ = 'listings'
 
     listing_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(500))
+    price = db.Column(db.Integer)
+    last_modified_date = db.Column(db.DateTime)
     address = db.Column(db.String(200), unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     price = db.Column(db.Integer)
-    description = db.Column(db.String(500))
-    rating = db.Column(db.Integer)
-    reviews = db.relationship('Review', backref='listing', lazy=True)
-    picture = db.Column(db.String(200))
-    start_date = db.Column(db.DateTime)
-    end_date = db.Column(db.DateTime)
-    transactions = db.relationship('Transaction', backref='listing')
+    reviews = db.relationship('Review')
+    bookings = db.relationship('Booking')
 
 
 class ReviewStarsEnum(enum.IntEnum):
@@ -75,12 +77,12 @@ class Review(db.Model):
     """
     A review model such that Users can leave reviews on listings.
 
-    review_id -- id for the review model, the primary_key
-    user_id -- the id of the user posting this review
-    text -- the text of the review
-    stars -- star rating of the review
-    time -- time the review was posted
-    listing -- the listing this review was posted on
+    review_id: id for the review model, the primary_key
+    user_id: the id of the user posting this review
+    text: the text of the review
+    star: star rating of the review
+    date: time the review was posted
+    listing: the listing this review was posted on
     """
     __tablename__ = 'reviews'
 
@@ -89,37 +91,30 @@ class Review(db.Model):
                         nullable=False)
     text = db.Column(db.String(2000), nullable=False)
     stars = db.Column(db.Enum(ReviewStarsEnum), nullable=False)
-    time = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
     listing = db.Column(db.Integer, db.ForeignKey('listings.listing_id'))
 
 
-class Transaction(db.Model):
+class Booking(db.Model):
     """
-        A Transaction object represents a succcessful transaction to rent a
+        A Booking object represents a booking to rent a
         property listing.
 
         Includes the following properties:
 
-        listing_id: Numeric ID of the property being rented
+        booking_id: Booking ID
         user_id: Numeric ID of the user renting the property
+        listing_id: ID of the listing trying to be booked
         price: The price the listing was rented for
-        user_balance: The amount of money the user has
-                      remaining after the transaction
-        transac_time: The time at which the transaction occurred
-        trans_id: Numeric ID of the processed transaction
-        currency: The currency the user used to pay qB&B
+        date: The time at which the booking is posted
     """
-
     __tablename__ = 'transactions'
 
-    listing_id = db.Column(db.Integer, primary_key=True)
+    booking_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    listing_id = db.Column(db.Integer, db.ForeignKey('reviews.review_id'))
     price = db.Column(db.Integer, nullable=False)
-    user_balance = db.Column(db.Integer, db.ForeignKey('users.user_balance'))
-    transaction_time = db.Column(db.DateTime,
-                                 default=datetime.datetime.utcnow())
-    transaction_id = db.Column(db.Integer, unique=True, nullable=True)
-    currency = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
 
 
 db.create_all()
