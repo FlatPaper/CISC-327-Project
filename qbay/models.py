ed8f140a02a7907d89e1a9e3d677df93c4742b46
FlatPaper <20js30@queensu.ca>
2022-10-05 21:30:15 -0400

    Feature/16 register (#23)
    * Update models to fix one-to-many problems
    
    * Implement the register function
    
    * Update requirements.txt
    
    * Update models.py to pass flake8 build tests
    
    * Add first 3 unit tests for register function and fix flake8 build issues
    
    * Add space around arithmetic operator
    
    * Complete all unit tests for the register function
    
    * Update conftest as requested

--- qbay/models.py
+++ qbay/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 import datetime
 import enum
+import re
 
 
 db = SQLAlchemy(app)
@@ -24,14 +25,18 @@ class User(db.Model):
     __tablename__ = 'users'
 
     user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    billing_address = db.Column(db.String(120), nullable=False)
-    postal_code = db.Column(db.String(7), nullable=False)
+    password = db.Column(db.String(128))
+    billing_address = db.Column(db.String(120), nullable=True)
+    postal_code = db.Column(db.String(7), nullable=True)
     balance = db.Column(db.Integer, nullable=False)
-    listings = db.relationship('Listing')
-    reviews = db.relationship('Review')
-    bookings = db.relationship('Booking')
+    listings = db.relationship('Listing', backref='user')
+    reviews = db.relationship('Review', backref='user')
+    bookings = db.relationship('Booking', backref='user')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 
 class Listing(db.Model):
@@ -59,9 +64,13 @@ class Listing(db.Model):
     last_modified_date = db.Column(db.DateTime)
     address = db.Column(db.String(200), unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    # 'user' property defined in User.listings via backref
     price = db.Column(db.Integer)
-    reviews = db.relationship('Review')
-    bookings = db.relationship('Booking')
+    reviews = db.relationship('Review', backref='listing')
+    bookings = db.relationship('Booking', backref='listing')
+
+    def __repr__(self):
+        return '<Listing {}>'.format(self.listing_id)
 
 
 class ReviewStarsEnum(enum.IntEnum):
@@ -87,12 +96,16 @@ class Review(db.Model):
     __tablename__ = 'reviews'
 
     review_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'),
-                        nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    # 'user' property defined in User.listings via backref
     text = db.Column(db.String(2000), nullable=False)
     stars = db.Column(db.Enum(ReviewStarsEnum), nullable=False)
     date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
-    listing = db.Column(db.Integer, db.ForeignKey('listings.listing_id'))
+    listing_id = db.Column(db.Integer, db.ForeignKey('listings.listing_id'))
+    # 'listing' property defined in Listing.reviews via backref
+
+    def __repr__(self):
+        return '<Review {}>'.format(self.review_id)
 
 
 class Booking(db.Model):
@@ -112,9 +125,71 @@ class Booking(db.Model):
 
     booking_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    listing_id = db.Column(db.Integer, db.ForeignKey('reviews.review_id'))
+    # 'user' property defined in User.listings via backref
+    listing_id = db.Column(db.Integer, db.ForeignKey('listings.listing_id'))
+    # 'listing' property defined in Listings.bookings via backref
     price = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
 
+    def __repr__(self):
+        return '<Booking {}>'.format(self.booking_id)
+
 
 db.create_all()
+
+EMAIL_REGEX = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.["
+                         r"-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t "
+                         r"-~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.["
+                         r"-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+
+
+def register(username: str, email: str, password: str):
+    # Check for empty email and password fields
+    if len(email) == 0:
+        return False, "Email cannot be empty."
+    if len(password) == 0:
+        return False, "Password cannot be empty."
+
+    # Check that email matches RFC 5322 constraints with a very long regex
+    if not EMAIL_REGEX.match(email):
+        return False, "Email does not follow addr-spec defined in RFC 5322/"
+
+    # Check for password complexity constraints
+    if len(password) < 6:
+        return False, "Password is too short."
+    if not any(ch.isupper() for ch in password):
+        return False, "The password does not contain any uppercase characters."
+    if not any(ch.islower() for ch in password):
+        return False, "The password does not contain any lowercase characters."
+    if not any(not ch.isalnum() for ch in password):
+        return False, "The password does not contain any special characters."
+
+    # Check for username constraints
+    if len(username) == 0:
+        return False, "The username is empty."
+    if len(username) <= 2 or len(username) >= 20:
+        error_info = "The username must be longer than 2 characters and " \
+                     "shorter than 20 characters. "
+        return False, error_info
+    if not all(ch.isalnum() or ch.isspace() for ch in username):
+        error_info = "The username contains characters that are not " \
+                     "alphanumeric or a space. "
+        return False, error_info
+    if username[0] == ' ':
+        return False, "The username cannot contain a space as its prefix."
+    if username[len(username) - 1] == ' ':
+        return False, "The username cannot contain a space as its suffix."
+
+    # Check if email has been used
+    email_query = User.query.filter_by(email=email).all()
+    if len(email_query) > 0:
+        return False, "This email has been used already."
+
+    user = User(username=username, email=email, password=password,
+                balance=100)
+
+    db.session.add(user)
+
+    db.session.commit()
+
+    return True, "User has been created!"
---
+++ qbay_test/test_models.py
@@ -0,0 +1,248 @@
+from qbay.models import register
+from qbay.models import User
+
+
+def test_r1_1_user_register():
+    """
+    When creating a user, check that the email field and password field
+    cannot be empty.
+    Assert 1: Check that email cannot be empty.
+    Assert 2: Check that password cannot be empty.
+    Assert 3: Check that both cannot be empty.
+    """
+    assert register(
+        username="test user",
+        email="",
+        password="test_password")[0] is False
+    assert register(
+        username="test user",
+        email="test@gmail.com",
+        password="")[0] is False
+    assert register(
+        username="test user",
+        email="",
+        password="")[0] is False
+    assert register(
+        username="pass user",
+        email="pass_empty@gmail.com",
+        password="Pass_pwd")[0] is True
+
+
+def test_r1_2_user_register():
+    """
+    Register 2 users, then use a set and compare list vs set size
+    to determine that all user_id's are unique.
+    """
+    register(
+        username="Test user 1",
+        email="test_user_id_1@gmail.com",
+        password="Test_user_1_pwd")
+    register(
+        username="Test user 2",
+        email="test_user_id_2@gmail.com",
+        password="Test_user_2_pwd")
+    user_ids: list[int] = User.query.order_by(User.user_id)\
+        .with_entities(User.user_id).all()
+    assert len(user_ids) == len(set(user_ids))
+
+
+def test_r1_3_user_register():
+    """
+    When creating a user, determine if the email follows the
+    constraints of RFC 5322.
+    """
+    assert register(
+        username="rfc pass 1",
+        email="name.surname@gmail.com",
+        password="RFC_pass1_pwd")[0] is True
+    assert register(
+        username="rfc pass 2",
+        email="testUsername123@yahoo.co.uk",
+        password="RFC_pass2_pwd")[0] is True
+    assert register(
+        username="rfc fail 1",
+        email="name.Surname@...ca",
+        password="RFC_fail1_pwd")[0] is False
+    assert register(
+        username="rfc fail 2",
+        email="...@gmail.com",
+        password="RFC_fail2_pwd")[0] is False
+
+
+def test_r1_4_user_register():
+    """
+    While creating the user, ensure that the password meets the required
+    complexity.
+    """
+    # Check minimum length test case
+    assert register(
+        username="pwd fail 1",
+        email="pwdTestFail1@gmail.com",
+        password="Sh0rt"
+    )[0] is False
+    # Check password has no upper case
+    assert register(
+        username="pwd fail 2",
+        email="pwdTestFail2@gmail.com",
+        password="n0_upper_case"
+    )[0] is False
+    # Check password has no lower case
+    assert register(
+        username="pwd fail 3",
+        email="pwdTestFail3@gmail.com",
+        password="N0_LOWER_CASE"
+    )[0] is False
+    # Check password has no special character
+    assert register(
+        username="pwd fail 4",
+        email="pwdTestFail4@gmail.com",
+        password="NoSpecialCharacters"
+    )[0] is False
+    # Check for a pass case where we meet all requirements
+    assert register(
+        username="pwd pass 1",
+        email="pwdTestPass1@gmail.com",
+        password="G00d#Password!"
+    )[0] is True
+
+
+def test_r1_5_user_register():
+    """
+    Check for username being non-empty, alphanumeric-only,
+    and space allowed as long as it's not in the prefix or suffix.
+    """
+    # Check for username that is empty
+    assert register(
+        username="",
+        email="r1_5_1_fail@gmail.com",
+        password="G00d#Password!"
+    )[0] is False
+    # Check for non-alphanumeric-only usernames
+    assert register(
+        username="HEEHAW#143",
+        email="r1_5_2_fail@gmail.com",
+        password="G00d#Password!"
+    )[0] is False
+    # Check for prefix space in username
+    assert register(
+        username=" prefix space",
+        email="r1_5_3_fail@gmail.com",
+        password="G00d#Password!"
+    )[0] is False
+    # Check for suffix space in username
+    assert register(
+        username="suffix space ",
+        email="r1_5_4_fail@gmail.com",
+        password="G00d#Password!"
+    )[0] is False
+    # Check that a good username passes
+    assert register(
+        username="good Username",
+        email="r1_5_1_pass@gmail.com",
+        password="G00d#Password!"
+    )[0] is True
+
+
+def test_r1_6_user_register():
+    """
+    Test that username length should be longer than 2 characters
+    and shorter than 20 characters.
+    """
+    # Check for 2 or less characters
+    assert register(
+        username="Ba",
+        email="r1_6_1_fail@gmail.com",
+        password="G00d#Password!"
+    )[0] is False
+    # Check for 20 or more characters
+    assert register(
+        username="qwertyuiopasdfghjklz",
+        email="r1_6_2_fail@gmail.com",
+        password="G00d#Password!"
+    )[0] is False
+    # Check for a case in between that SHOULD pass
+    assert register(
+        username="Good Username",
+        email="r1_6_1_pass@gmail.com",
+        password="G00d#Password!"
+    )[0] is True
+
+
+def test_r1_7_user_register():
+    """
+    When creating a user, registering should fail
+    if an email has been used already.
+    """
+    assert register(
+        username="Email Used 1",
+        email="r1_7_1_test@gmail.com",
+        password="G00d#Password!"
+    )[0] is True
+    # Check that reusing "r1_7_1_test@gmail.com" will fail since we
+    # just created a user with that email in the db.
+    assert register(
+        username="Email Used 2",
+        email="r1_7_1_test@gmail.com",
+        password="G00d#Password!"
+    )[0] is False
+
+    # And finally, as a sanity check, scan through the database to make sure
+    # all emails are unique using a set
+    user_emails: list[str] = User.query.order_by(User.email)\
+        .with_entities(User.email).all()
+    assert len(user_emails) == len(set(user_emails))
+
+
+# noinspection PyComparisonWithNone
+def test_r1_8_user_register():
+    """
+    When a user is created, shipping address should be left blank
+    (essentially, NULL)
+    """
+    assert register(
+        username="Shipping Null",
+        email="r1_8_1_pass@gmail.com",
+        password="G00d#Password!"
+    )[0] is True
+
+    # Compare the list size of all users vs
+    # all users that have a blank billing address
+    user_address = User.query.order_by(User.user_id)\
+        .filter(User.billing_address.is_(None)).all()
+    users = User.query.order_by(User.user_id).all()
+    assert len(users) == len(user_address)
+
+
+def test_r1_9_user_register():
+    """
+    When a user is created, postal code should be left blank
+    (essentially, NULL)
+    """
+    assert register(
+        username="Postal Null",
+        email="r1_9_1_pass@gmail.com",
+        password="G00d#Password!"
+    )[0] is True
+
+    # Assert all postal codes are blank to this point
+    user_postal_codes = User.query.order_by(User.user_id)\
+        .filter(User.postal_code.is_(None)).all()
+    users = User.query.order_by(User.user_id).all()
+    assert len(users) == len(user_postal_codes)
+
+
+def test_r1_10_user_register():
+    """
+    When a user is created successfully, their balance should
+    be initialized to 100.
+    """
+    assert register(
+        username="Balance 100",
+        email="r1_10_1_pass@gmail.com",
+        password="G00dPassword#!"
+    )[0] is True
+
+    # Check all users that we've added have a starting balance of 100
+    users: list[int, int] = User.query.order_by(User.user_id)\
+        .with_entities(User.user_id, User.balance)
+    assert (user_info[1] == 100 for user_info in users)
