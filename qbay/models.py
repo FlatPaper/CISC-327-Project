e2e6160e5bfb0f20603f9f90f9ad7eb7229fd9b8
Gansito52 <68562890+Gansito52@users.noreply.github.com>
2022-10-06 01:07:08 -0400

    Updated models.py
    Should abide by pep8 guidelines now

--- qbay/models.py
+++ qbay/models.py
@@ -239,9 +239,46 @@ def login(email: str, password: str):
 
     return match_accounts[0], "This account exists."
 
+def create_listing(title: str, description: str, price: int,
+                   address: str, user_id: int):
+    if any(not x.isalnum() or not x.isspace() for x in title):
+        return False, "The title of the listing must be alphanumeric."
+    if title[0] == " " or title[-1] == " ":
+        return False, "Space is allowed if it's not a prefix or suffix."
+    if len(title) > 80:
+        return False, "Length of title must be 80 or less characters."
+    if len(description < 20) or len(description > 2000):
+        return False, "Description length must be between 20-2000 characters."
+    if len(description) <= len(title):
+        return False, "Description length must be longer than the title."
+    if price < 10 or price > 10000:
+        return False, "Price must be between [10, 10000]."
+
+    date = datetime.today()
+
+    user = User.query.get(user_id)
+    if user is None:
+        return False, "User id does not exist!"
+    user_email = user.email
+
+    listings = User.query.get(user_id).listings
+    if any(title == product.title for product in listings):
+        return False, "Listings cannot have the same title."
+
+    listing_obj = Listing(title=title, description=description,
+                          price=price, last_modified_date=date,
+                          address=address, user_id=user_id,
+                          user_email=user_email)
+
+    db.session.add(listing_obj)
+
+    db.session.commit()
+
+    return True
+
 def validate_title(title: str):
     if not title.isalnum():
-        return False, "Title of the product must be alphanumeric, and spaces are allowed only if they are neither prefix nor suffix."
+        return False, "Title of the product must be alphanumeric."
     
     if (len(title) > 80):
         return False, "Title of the product must be 80 characters or less."
@@ -253,10 +290,10 @@ def validate_title(title: str):
 
 def validate_description(description: str):
     if (len(description) < 20 and len(description) > 2000):
-        return False, "Description of the listing must be between 20 and 2000 characters."
+        return False, "Description length must be between 20-2000 characters."
 
     if (len(description) < len(title)):
-        return False, "Description of the listing must be longer than the title."
+        return False, "Description must be longer than the title."
 
     return True, "Description meets constraints."
 
@@ -279,20 +316,21 @@ def validate_date(date: datetime):
     low = date(2021, 1, 2)
     high = date(2025, 1, 2)
     if (date < low and date > high):
-        return False, "New date must be after 2021-01-02 and before 2025-01-02."
+        return False, "New date must be between 2021-01-02 and 2025-01-02."
     
     return True, "Date was modified."
 
-def update_listing(listing_id: int, title = None, description = None, price = None, address = None):
+def update_listing(listing_id: int, title = None,
+    description = None, price = None, address = None):
     """
         This function updates the attributes of the posted listing.
-        If any of the inputs are not given, then automatically assigns them None and updates the
-        remaining attributes.
+        If any of the inputs are not given, then automatically assigns them
+        None and updates the remaining attributes.
         If none are given, no attributes are updated.
     """
     listing = Listing.query.get(listing_id)
 
-    #Each None ensures that if an input variable is missing, it does not change.
+    #Each None ensures that if a missing input variable does not change.
     if title is not None:
         # Validate title constraints
         flag, msg = validate_title(title)
