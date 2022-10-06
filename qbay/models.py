fbe1f33a1ebd16de112d7ccef8762ceb90132485
calebchiu14 <calebchiu1@hotmail.com>
2022-10-06 01:33:48 -0400

    Merge branch 'main' of https://github.com/FlatPaper/CISC-327-Project

--- qbay/models.py
+++ qbay/models.py
@@ -1,8 +1,9 @@
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
 
-import datetime
+from datetime import datetime
 import enum
+import re
 
 
 db = SQLAlchemy(app)
@@ -24,20 +25,23 @@ class User(db.Model):
     __tablename__ = 'users'
 
     user_id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    billing_address = db.Column(db.String(120), nullable=False)
-    postal_code = db.Column(db.String(7), nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String)
+    billing_address = db.Column(db.String, nullable=True)
+    postal_code = db.Column(db.String, nullable=True)
     balance = db.Column(db.Integer, nullable=False)
-    listings = db.relationship('Listing')
-    reviews = db.relationship('Review')
-    bookings = db.relationship('Booking')
+    listings = db.relationship('Listing', backref='user')
+    reviews = db.relationship('Review', backref='user')
+    bookings = db.relationship('Booking', backref='user')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 
 class Listing(db.Model):
     """
     An Object to Express a property listing.\n
-
     Properties:\n
     listing_id: The integer ID for the listing\n
     title: title of the listing
@@ -53,15 +57,19 @@ class Listing(db.Model):
     __tablename__ = 'listings'
 
     listing_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-    description = db.Column(db.String(500))
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
     price = db.Column(db.Integer)
     last_modified_date = db.Column(db.DateTime)
-    address = db.Column(db.String(200), unique=True)
+    address = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    price = db.Column(db.Integer)
-    reviews = db.relationship('Review')
-    bookings = db.relationship('Booking')
+    user_email = db.Column(db.String)
+    # 'user' property defined in User.listings via backref
+    reviews = db.relationship('Review', backref='listing')
+    bookings = db.relationship('Booking', backref='listing')
+
+    def __repr__(self):
+        return '<Listing {}>'.format(self.listing_id)
 
 
 class ReviewStarsEnum(enum.IntEnum):
@@ -76,7 +84,6 @@ class ReviewStarsEnum(enum.IntEnum):
 class Review(db.Model):
     """
     A review model such that Users can leave reviews on listings.
-
     review_id: id for the review model, the primary_key
     user_id: the id of the user posting this review
     text: the text of the review
@@ -87,21 +94,23 @@ class Review(db.Model):
     __tablename__ = 'reviews'
 
     review_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'),
-                        nullable=False)
-    text = db.Column(db.String(2000), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    # 'user' property defined in User.listings via backref
+    text = db.Column(db.String, nullable=False)
     stars = db.Column(db.Enum(ReviewStarsEnum), nullable=False)
-    date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
-    listing = db.Column(db.Integer, db.ForeignKey('listings.listing_id'))
+    date = db.Column(db.DateTime)
+    listing_id = db.Column(db.Integer, db.ForeignKey('listings.listing_id'))
+    # 'listing' property defined in Listing.reviews via backref
+
+    def __repr__(self):
+        return '<Review {}>'.format(self.review_id)
 
 
 class Booking(db.Model):
     """
         A Booking object represents a booking to rent a
         property listing.
-
         Includes the following properties:
-
         booking_id: Booking ID
         user_id: Numeric ID of the user renting the property
         listing_id: ID of the listing trying to be booked
@@ -112,9 +121,211 @@ class Booking(db.Model):
 
     booking_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    listing_id = db.Column(db.Integer, db.ForeignKey('reviews.review_id'))
+    # 'user' property defined in User.listings via backref
+    listing_id = db.Column(db.Integer, db.ForeignKey('listings.listing_id'))
+    # 'listing' property defined in Listings.bookings via backref
     price = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+    date = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<Booking {}>'.format(self.booking_id)
 
 
 db.create_all()
+
+EMAIL_REGEX = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.["
+                         r"-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t "
+                         r"-~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.["
+                         r"-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+
+
+POSTAL_REGEX = re.compile(r"^[A-Z]\d[A-Z]\d[A-Z]\d$")
+
+
+def validate_email(email: str):
+    # Check for empty email field
+    if len(email) == 0:
+        return False, "Email cannot be empty."
+
+    # Check that email matches RFC 5322 constraints with a very long regex
+    if not EMAIL_REGEX.match(email):
+        return False, "Email does not follow addr-spec defined in RFC 5322."
+
+    return True, "Email is in the correct format."
+
+
+def validate_password(password: str):
+    # Check for empty email and password fields
+    if len(password) == 0:
+        return False, "Password cannot be empty."
+
+    # Check for password complexity constraints
+    if len(password) < 6:
+        return False, "Password is too short."
+    if not any(ch.isupper() for ch in password):
+        return False, "The password does not contain any uppercase characters."
+    if not any(ch.islower() for ch in password):
+        return False, "The password does not contain any lowercase characters."
+    if not any(not ch.isalnum() for ch in password):
+        return False, "The password does not contain any special characters."
+
+    return True, "Password meets the constraints."
+
+
+def validate_postalcode(postal_code: str):
+    # Check if postal code field is empty
+    if len(postal_code) == 0:
+        return False, "Postal code cannot be empty."
+
+    # Check that postal code is a valid Canadian postal code
+    if not POSTAL_REGEX.match(postal_code):
+        return False, "Postal code is not a valid Canadian postal code."
+
+    return True, "Valid Canadian postal code given."
+
+
+def validate_username(username: str):
+    # Check for username constraints
+    if len(username) == 0:
+        return False, "The username is empty."
+    if len(username) <= 2 or len(username) >= 20:
+        error_info = "The username must be longer than 2 characters and " \
+                     "shorter than 20 characters. "
+        return False, error_info
+    if not all(ch.isalnum() or ch.isspace() for ch in username):
+        error_info = "The username contains characters that are not " \
+                     "alphanumeric or a space. "
+        return False, error_info
+    if username[0] == ' ':
+        return False, "The username cannot contain a space as its prefix."
+    if username[len(username) - 1] == ' ':
+        return False, "The username cannot contain a space as its suffix."
+
+    return True, "Username meets the constraints."
+
+
+def register(username: str, email: str, password: str):
+    # Validate the email constraints
+    flag, msg = validate_email(email)
+    if flag is False:
+        return flag, msg
+
+    # Validate the password constraints
+    flag, msg = validate_password(password)
+    if flag is False:
+        return flag, msg
+
+    # Check for username constraints
+    flag, msg = validate_username(username)
+    if flag is False:
+        return flag, msg
+
+    # Check if email has been used
+    email_query = User.query.filter_by(email=email).all()
+    if len(email_query) > 0:
+        return False, "This email has been used already."
+
+    user = User(username=username, email=email, password=password,
+                balance=100)
+
+    db.session.add(user)
+
+    db.session.commit()
+
+    return True, "User has been created!"
+
+
+def login(email: str, password: str):
+    # Validate email constraints
+    flag, msg = validate_email(email)
+    if flag is False:
+        return flag, msg
+
+    # Validate password constraints
+    flag, msg = validate_password(password)
+    if flag is False:
+        return flag, msg
+
+    match_accounts = User.query.filter_by(email=email, password=password).all()
+    if len(match_accounts) < 1:
+        return None, "This account does not exist"
+    # Check for an "impossible" situation for debugging purposes in the future
+    if len(match_accounts) > 1:
+        return None, "There are more than one accounts with this email!"
+
+    return match_accounts[0], "This account exists."
+
+
+def update_user_profile(user_id: int, username=None, email=None,
+                        billing_adress=None, postal_code=None):
+
+    user = User.query.get(user_id)
+
+    if username is not None:
+        # Check new username for constraints
+        flag, msg = validate_username(username)
+        if flag is False:
+            return flag, msg
+
+        user.username = username
+
+    if email is not None:
+        # Validate the email constraints
+        flag, msg = validate_email(email)
+        if flag is False:
+            return flag, msg
+
+        user.email = email
+
+    if billing_adress is not None:
+        user.billing_adress = billing_adress
+
+    if postal_code is not None:
+        # Validate postal code
+        flag, msg = validate_email(postal_code)
+        if flag is False:
+            return flag, msg
+
+        user.postal_code = postal_code
+
+    db.session.commit()
+
+    return True, "Profile has been updated!"
+
+
+def create_listing(title: str, description: str, price: int,
+                   address: str, user_id: int):
+    if not all(x.isalnum() or x.isspace() for x in title):
+        return False, "The title of the listing must be alphanumeric."
+    if title[0] == " " or title[-1] == " ":
+        return False, "Space is allowed if it's not a prefix or suffix."
+    if len(title) > 80:
+        return False, "Length of title must be 80 or less characters."
+    if len(description) < 20 or len(description) > 2000:
+        return False, "Description length must be between 20-2000 characters."
+    if len(description) <= len(title):
+        return False, "Description length must be longer than the title."
+    if price < 10 or price > 10000:
+        return False, "Price must be between [10, 10000]."
+
+    date = datetime.today()
+
+    user = User.query.get(user_id)
+    if user is None:
+        return False, "User id does not exist!"
+    user_email = user.email
+
+    listings = User.query.get(user_id).listings
+    if any(title == product.title for product in listings):
+        return False, "Listings cannot have the same title."
+
+    listing_obj = Listing(title=title, description=description,
+                          price=price, last_modified_date=date,
+                          address=address, user_id=user_id,
+                          user_email=user_email)
+
+    db.session.add(listing_obj)
+
+    db.session.commit()
+
+    return True, "Listing was created!"
