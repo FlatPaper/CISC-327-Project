a5da3b0ba70aba1f827c6c56d8e01d2c5b7e2967
davidKro <19dsk3@queensu.ca>
2022-10-20 15:32:03 -0400

    fixed pep8 formating

--- qbay/__main__.py
+++ qbay/__main__.py
@@ -1,18 +1,20 @@
 from qbay.models import create_listing
 
+
 def create_listing_page():
-    #loop till they choose to exit
+    # loop till they choose to exit
     while True:
         selection = input('Press 1 to make a listing or 2 to exit the page')
         if selection == 1:
             title = input('Enter the title of your listing: ')
-            address = input ('Enter the address of your property: ')
+            address = input('Enter the address of your property: ')
             description = input('Enter the description of your property: ')
             price = input('Enter the price per night (whole dollars): ')
             try:
                 price = int(price)
-                #replace the 1 with the currently logged in user
-                flag, msg = create_listing(title,description,price,address,1)
+                # replace the 1 with the currently logged in user
+                flag, msg = create_listing(title, description, price, address,
+                                           1)
                 if flag is False:
                     print('Listing not created')
                     print(msg)
@@ -26,7 +28,7 @@ def create_listing_page():
                 print('Price is not an integer')
 
         elif selection == 2:
-            #replace the break with user home page function
+            # replace the break with user home page function
             break 
 
     
