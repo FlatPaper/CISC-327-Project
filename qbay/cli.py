7cb85140370c9dedfee9f7aef208386e46171806
Gansito52 <68562890+Gansito52@users.noreply.github.com>
2022-10-20 23:09:05 -0400

    Fixes more pep8 formatting errors

--- qbay/cli.py
+++ qbay/cli.py
@@ -2,18 +2,18 @@ from qbay.models import update_listing
 
 
 def update_listing_page():
-    #loop until they want to exit
+    '# loop until they want to exit'
     while True:
-        listing = ('Please enter the listing id of the listing you want to '
-            'update: ')
-        selection = input('Press 1 to update the title, 2 to update the ' 
-            'description, 3 to update the price, 4 to update the address, '
-            'and 5 to exit.')
+        listing = input('Please enter the listing id of the listing you want to '
+                        'update: ')
+        selection = input('Press 1 to update the title, 2 to update the '
+                          'description, 3 to update the price, 4 to update the address, '
+                          'and 5 to exit.')
         if selection == 1:
             title = input('Enter the new title for your listing: ')
         elif selection == 2:
-            description = input('Enter the new description for your ' 
-                'listing: ')
+            description = input('Enter the new description for your '
+                                'listing: ')
         elif selection == 3:
             price = input('Enter the new price per night (in whole dollars: ')
             try:
