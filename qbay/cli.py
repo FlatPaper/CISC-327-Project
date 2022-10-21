b191b5cddf167c0d82bea8aa47c5eea2891afb2c
Gansito52 <68562890+Gansito52@users.noreply.github.com>
2022-10-20 23:12:07 -0400

    More pep8 fixes

--- qbay/cli.py
+++ qbay/cli.py
@@ -4,11 +4,11 @@ from qbay.models import update_listing
 def update_listing_page():
     '# loop until they want to exit'
     while True:
-        listing = input('Please enter the listing id of the listing you want to '
-                        'update: ')
+        listing = input('Please enter the listing id of the listing you want '
+                        'to update: ')
         selection = input('Press 1 to update the title, 2 to update the '
-                          'description, 3 to update the price, 4 to update the address, '
-                          'and 5 to exit.')
+                          'description, 3 to update the price, 4 to update '
+                          'the address, and 5 to exit.')
         if selection == 1:
             title = input('Enter the new title for your listing: ')
         elif selection == 2:
