1f8d4dbb09614de5c44cfdbd990066fb257b1a48
FlatPaper <diaxaxx@gmail.com>
2022-10-23 09:46:53 -0400

    Merge main,py from main branch, add update_listing_page command

--- qbay/__main__.py
+++ qbay/__main__.py
@@ -1,5 +1,6 @@
 from qbay.models import *
-from qbay.cli import register_page, login_page, update_listing_page
+from qbay.cli import register_page, login_page, update_user_page, \
+    create_listing_page, update_listing_page
 
 
 def main():
@@ -8,15 +9,32 @@ def main():
     while True:
         if logged_in:
             selection = input(
-                "Your options are as follows:"
-                "Please press 1 to create a listing."
-                "Please press 2 to update a listing."
+                "Your options are as follows:\n"
+                "Please press 1 to create a listing.\n"
+                "Please press 2 to update a listing.\n"
                 "Please press 3 to update your user profile page.\n"
-                "Please press 4 to log out."
+                "Please press 4 to log out.\n"
             )
             selection = selection.strip()
+            if selection == "1":
+                user_email = current_user[0]
+                user_id = User.query.filter_by(
+                    email=user_email).all()[0].user_id
+                create_listing_page(user_id)
             if selection == "2":
-                update_listing_page()
+                user_email = current_user[0]
+                user_id = User.query.filter_by(
+                    email=user_email).all()[0].user_id
+                flag, msg = update_listing_page(user_id)
+                print(msg)
+            if selection == "3":
+                user = User.query.filter_by(email=current_user[0]).all()[0]
+                result = update_user_page(user.user_id)
+                if result[0]:
+                    print("Successfully updated profile!")
+                else:
+                    print("Unable to update profile!")
+                    print(result[1])
             if selection == "4":
                 logged_in = False
                 current_user = None
@@ -39,7 +57,9 @@ def main():
                     print("Login failed!")
             if selection == "2":
                 register_page()
+            if selection == "3":
+                break
 
 
-if name == 'main':
+if __name__ == '__main__':
     main()
