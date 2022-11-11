dfac3033db8410b509cb502e50550c75967bc69d
Gansito52 <68562890+Gansito52@users.noreply.github.com>
2022-11-11 11:19:42 -0500

    Add files via upload

--- qbay_test/frontend/test_update_listing/test_update_listing.py
+++ qbay_test/frontend/test_update_listing/test_update_listing.py
@@ -9,22 +9,30 @@ current_folder = Path(__file__).parent
 def test_login():
     """capsys -- object created by pytest to 
     capture stdout and stderr"""
-
+    
     for i in range(1, 12):
+
         # read expected in/out
         expected_in = open(current_folder.joinpath(
-            'test_update_listing_' + str(i) + '.in'))
+            'test_update_listing_'+str(i)+'.in'))
         expected_out = open(current_folder.joinpath(
-            'test_update_listing_' + str(i) + '.out')).read()
+            'test_update_listing_'+str(i)+'.out')).read()
+    
+        expected_out = expected_out.replace('\r', '')
+        output = output.replace('\r', '')
+        expected_out = expected_out.replace('\n', '')
+        output = output.replace('\n', '')
+        expected_out = expected_out.replace(' ', '')
+        output = output.replace(' ', '')
 
         print(expected_out)
 
-        # pip the input
-        output = subprocess.run(
-            ['python', '-m', 'qbay'],
-            stdin=expected_in,
-            capture_output=True,
-        ).stdout.decode()
+    # pip the input
+    output = subprocess.run(
+        ['python', '-m', 'qbay'],
+        stdin=expected_in,
+        capture_output=True,
+    ).stdout.decode()
 
-        print('outputs', output)
-        assert output.strip() == expected_out.strip()
+    print('outputs', output)
+    assert output.strip() == expected_out.strip() 
