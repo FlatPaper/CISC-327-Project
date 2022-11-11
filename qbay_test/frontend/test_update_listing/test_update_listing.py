9b54b4f1697ec15476ee2107685c0b57ea4a5449
FlatPaper <diaxaxx@gmail.com>
2022-11-11 11:32:55 -0500

    Update test listing to have a registered account

--- qbay_test/frontend/test_update_listing/test_update_listing.py
+++ qbay_test/frontend/test_update_listing/test_update_listing.py
@@ -1,23 +1,37 @@
 from os import popen
 from pathlib import Path
 import subprocess
+from qbay.models import register
 
 # get expected input/output file
 current_folder = Path(__file__).parent
 
 
 def test_login():
-    """capsys -- object created by pytest to 
+    """capsys -- object created by pytest to
     capture stdout and stderr"""
-    
-    for i in range(1, 12):
 
+    register(username="Temporary 1", email="pass_empty@gmail.com",
+             password="Pass_pwd")
+
+    for i in range(1, 12):
         # read expected in/out
         expected_in = open(current_folder.joinpath(
-            'test_update_listing_'+str(i)+'.in'))
+            'test_update_listing_' + str(i) + '.in'))
         expected_out = open(current_folder.joinpath(
-            'test_update_listing_'+str(i)+'.out')).read()
-    
+            'test_update_listing_' + str(i) + '.out')).read()
+
+        print(expected_out)
+
+        # pip the input
+        output = subprocess.run(
+            ['python', '-m', 'qbay'],
+            stdin=expected_in,
+            capture_output=True,
+        ).stdout.decode()
+
+        print('outputs', output)
+
         expected_out = expected_out.replace('\r', '')
         output = output.replace('\r', '')
         expected_out = expected_out.replace('\n', '')
@@ -25,14 +39,4 @@ def test_login():
         expected_out = expected_out.replace(' ', '')
         output = output.replace(' ', '')
 
-        print(expected_out)
-
-    # pip the input
-    output = subprocess.run(
-        ['python', '-m', 'qbay'],
-        stdin=expected_in,
-        capture_output=True,
-    ).stdout.decode()
-
-    print('outputs', output)
-    assert output.strip() == expected_out.strip() 
+        assert output.strip() == expected_out.strip()
