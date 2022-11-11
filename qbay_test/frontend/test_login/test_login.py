1231f2b2937be29a08105b03709fdcde2a3a74f1
FlatPaper <diaxaxx@gmail.com>
2022-11-11 14:36:26 -0500

    Add boundary black box testing

--- qbay_test/frontend/test_login/test_login.py
+++ qbay_test/frontend/test_login/test_login.py
@@ -60,11 +60,6 @@ def test_login2():
     Login failed and login succeeded
     """
 
-    db.session.query(User).delete()
-    db.session.commit()
-    register(username="FlatPaper", email="good.email@gmail.com",
-             password="GoodPassword!")
-
     for i in range(9, 11):
         str_in = "test_cases/test_login" + str(i) + ".in"
         str_out = "test_cases/test_login" + str(i) + ".out"
@@ -93,3 +88,40 @@ def test_login2():
         output = output.replace(' ', '')
 
         assert output.strip() == expected_out.strip()
+
+
+def test_login3():
+    """
+    Boundary Value Robustness Testing
+    Put a limit on email and password lengths: Will it crash?
+    past 2000 characters each?
+    Should pass constraints with failure.
+    """
+    # Code to output big boundary test on username and password
+    # fin = open("test_cases/test_login11.in", "w")
+    # fin.write("1\n")
+    # email = "a" * 2000 + "@gmail.com"
+    # password = "G00dPassword!" * 154
+    # fin.write(email + '\n')
+    # fin.write(password + '\n')
+    # fin.close()
+
+    str_in = "test_cases/test_login11.in"
+    str_out = "test_cases/test_login11.out"
+    expected_in = open(current_folder.joinpath(str_in))
+    expected_out = open(current_folder.joinpath(str_out)).read()
+
+    output = subprocess.run(
+        ['python', '-m', 'qbay'],
+        stdin=expected_in,
+        capture_output=True,
+    ).stdout.decode()
+
+    expected_out = expected_out.replace('\r', '')
+    output = output.replace('\r', '')
+    expected_out = expected_out.replace('\n', '')
+    output = output.replace('\n', '')
+    expected_out = expected_out.replace(' ', '')
+    output = output.replace(' ', '')
+
+    assert output.strip() == expected_out.strip()
