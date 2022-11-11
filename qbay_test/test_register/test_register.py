69ac90cce8c73e3af9226163c1fccf5fa035490e
FlatPaper <diaxaxx@gmail.com>
2022-11-11 02:03:56 -0500

    Boundary Test

--- qbay_test/test_register/test_register.py
+++ qbay_test/test_register/test_register.py
@@ -86,3 +86,40 @@ def test_register2():
 
         assert output.strip() == expected_out.strip()
 
+
+def test_register3():
+    """
+    Boundary Value Robustness Testing
+    Put a limit on username and password lengths: Will it crash?
+    past 2000 characters each?
+    Should pass constraints with failure on username length.
+    """
+    fin = open("input_partition/test_register16.in", "w")
+    fin.write("2\n")
+    username = "a" * 2000
+    password = "G00dPassword!" * 154
+    fin.write("good.email15@gmail.com\n")
+    fin.write(username + '\n')
+    fin.write(password + '\n')
+    fin.write(password + '\n')
+    fin.close()
+
+    str_in = "input_partition/test_register16.in"
+    str_out = "input_partition/test_register16.out"
+    expected_in = open(current_folder.joinpath(str_in))
+    expected_out = open(current_folder.joinpath(str_out)).read()
+
+    output = subprocess.run(
+        ['python', '-m', 'qbay'],
+        stdin=expected_in,
+        capture_output=True,
+    ).stdout.decode()
+
+    expected_out = expected_out.replace('\r', '')
+    output = output.replace('\r', '')
+    expected_out = expected_out.replace('\n', '')
+    output = output.replace('\n', '')
+    expected_out = expected_out.replace(' ', '')
+    output = output.replace(' ', '')
+
+    assert output.strip() == expected_out.strip()
