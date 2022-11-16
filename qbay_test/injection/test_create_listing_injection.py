85c261cb9e118c05cbae9eb8449e60f920769abe
davidKro <93348200+davidKro@users.noreply.github.com>
2022-11-16 16:09:25 -0500

    Fixed flake8, backslashes, indents, no extra whitespace around operator

--- qbay_test/injection/test_create_listing_injection.py
+++ qbay_test/injection/test_create_listing_injection.py
@@ -8,11 +8,11 @@ def test_create_listing_injection_title():
     lines = f.readlines()
     f.close
     for line in lines:
-        create_listing(title=line,\
-            description="This is a sample description", \
-            price=50, \
-            address="123 queen st", \
-            user_id=1)
+        create_listing(title=line,
+                        description="This is a sample description",
+                        price=50,
+                        address="123 queen st",
+                        user_id=1)
 
 
 def test_create_listing_injection_description():
@@ -22,12 +22,12 @@ def test_create_listing_injection_description():
     f.close
     i = 0
     for line in lines:
-        title = 'Test Injection title'+ str(i)
-        create_listing(title=title, \
-            description=line, \
-            price=50, \
-            address="123 queen st", \
-            user_id=1)
+        title = 'Test Injection title' + str(i)
+        create_listing(title=title,
+                        description=line,
+                        price=50,
+                        address="123 queen st",
+                        user_id=1)
         i += 1
 
 
@@ -36,12 +36,12 @@ def test_create_listing_injection_price():
     f = open(current_folder.joinpath('injection.txt'), 'r')
     i = 0
     for line in f.readlines():
-        title = 'Test Injection title'+ str(i)
-        create_listing(title=title, \
-            description="This Cabin is quite nice and situated on a lake", \
-            price= line, \
-            address= "123 Avenue ave.", \
-            user_id=1)
+        title = 'Test Injection title' + str(i)
+        create_listing(title=title,
+                        description="This is a sample description",
+                        price=line,
+                        address="123 Avenue ave.",
+                        user_id=1)
         i += 1
     f.close()
     
@@ -51,12 +51,12 @@ def test_create_listing_injection_address():
     f = open(current_folder.joinpath('injection.txt'), 'r')
     i = 0
     for line in f.readlines():
-        title = 'Test Injection title'+ str(i)
-        create_listing(title=title, \
-        description="This Cabin is quite nice and situated on a lake", \
-        price=50, \
-        address=line, \
-        user_id=1)
+        title = 'Test Injection title' + str(i)
+        create_listing(title=title,
+                        description="This is a sample description",
+                        price=50,
+                        address=line,
+                        user_id=1)
         i += 1
     f.close()
 
@@ -66,11 +66,11 @@ def test_create_listing_injection_user():
     f = open(current_folder.joinpath('injection.txt'), 'r')
     i = 0
     for line in f.readlines():
-        title = 'Test Injection title'+ str(i)
-        create_listing(title=title, \
-            description="This Cabin is quite nice and situated on a lake", \
-            price=50, \
-            address= "123 Avenue ave.", \
-            user_id=line)
+        title = 'Test Injection title' + str(i)
+        create_listing(title=title,
+                        description="This is a sample description",
+                        price=50,
+                        address="123 Avenue ave.",
+                        user_id=line)
         i += 1
     f.close()
